0b6f15a0349152bf797f2fde0678863742b10c40
Oliver Bell <freshollie@gmail.com>
2018-06-19 03:56:19 +0200

    Added particles and card component

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -6,5 +6,134 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public title = 'app';
+
+  public myStyle: object = {};
+	public myParams: object = {};
+	public width: number = 100;
+	public height: number = 100;
+
+  ngOnInit() {
+    this.myParams = {
+      "particles": {
+        "number": {
+          "value": 38,
+          "density": {
+            "enable": true,
+            "value_area": 6814.5012586784715
+          }
+        },
+        "color": {
+          "value": "#00c2ff"
+        },
+        "shape": {
+          "type": "polygon",
+          "stroke": {
+            "width": 0,
+            "color": "#000000"
+          },
+          "polygon": {
+            "nb_sides": 7
+          },
+          "image": {
+            "src": "img/github.svg",
+            "width": 100,
+            "height": 100
+          }
+        },
+        "opacity": {
+          "value": 0.5,
+          "random": false,
+          "anim": {
+            "enable": false,
+            "speed": 0.5,
+            "opacity_min": 0.1,
+            "sync": false
+          }
+        },
+        "size": {
+          "value": 184.39238699953512,
+          "random": true,
+          "anim": {
+            "enable": false,
+            "speed": 40,
+            "size_min": 0.1,
+            "sync": false
+          }
+        },
+        "line_linked": {
+          "enable": false,
+          "distance": 48.10236182596568,
+          "color": "#ffffff",
+          "opacity": 0.8257572113457442,
+          "width": 1
+        },
+        "move": {
+          "enable": true,
+          "speed": 0.5,
+          "direction": "none",
+          "random": true,
+          "straight": false,
+          "out_mode": "out",
+          "bounce": false,
+          "attract": {
+            "enable": false,
+            "rotateX": 80.17060304327615,
+            "rotateY": 80.17060304327615
+          }
+        }
+      },
+      "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+          "onhover": {
+            "enable": false,
+            "mode": "repulse"
+          },
+          "onclick": {
+            "enable": false,
+            "mode": "push"
+          },
+          "resize": true
+        },
+        "modes": {
+          "grab": {
+            "distance": 400,
+            "line_linked": {
+              "opacity": 1
+            }
+          },
+          "bubble": {
+            "distance": 400,
+            "size": 40,
+            "duration": 2,
+            "opacity": 8,
+            "speed": 3
+          },
+          "repulse": {
+            "distance": 200,
+            "duration": 0.4
+          },
+          "push": {
+            "particles_nb": 4
+          },
+          "remove": {
+            "particles_nb": 2
+          }
+        }
+      },
+      "retina_detect": true
+    };
+    this.myStyle = {
+      'background': 'linear-gradient(to right, #a044ff, #6a3093)',
+      'position': 'fixed',
+      'width': '100%',
+      'height': '100%',
+      'z-index': -1,
+      'top': 0,
+      'left': 0,
+      'right': 0,
+      'bottom': 0,
+    };
+  }
 }
--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { QwackersComponent } from './qwackers/qwackers.component';
 import { MatCardModule } from '@angular/material/card';
+import { ParticlesModule } from 'angular-particle';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { MatCardModule } from '@angular/material/card';
   imports: [
     BrowserModule,
     MatCardModule,
+    ParticlesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
