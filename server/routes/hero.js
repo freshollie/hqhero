5bbc70742d8e22a374eeebd428784e11c43bca1f
Oliver Bell <freshollie@gmail.com>
2018-06-24 22:37:59 +0200

    Fixed route guard. Moved route guard out of routes

--- server/routes/hero.js
+++ server/routes/hero.js
@@ -1,31 +1,13 @@
+const hero = require("../data/hero.js");
+const guard = require("../authentication/guard.js");
+
 const { Router } = require("express");
 const router = Router();
-const basicAuth = require("express-basic-auth");
-const hero = require("../data/hero.js");
-const ip = require("ip");
-
 
 router.get("/status", function(req, res) {
     res.send(hero.getStatus());
 });
 
-
-const SUBNET_MASK = "255.0.0.0"
-const MY_SUBNET = ip.mask(ip.address(), SUBNET_MASK);
-const protected = function(req, res, next) {
-    /**
-     * Only allow local ip addresses through
-     */
-    const remote = req.ip.split(":").pop()
-    if (remote == "1" || 
-            remote == "127.0.0.1" || 
-            ip.mask(remote, SUBNET_MASK) == MY_SUBNET) {
-        return next();
-    } else {
-        console.log(`${remote} blocked. Not in ${MY_SUBNET}`);
-        res.status(403).send();
-    }
-}
 
 router.post("*", function(req, res, next) {
     const info = req.body.info;
@@ -35,37 +17,37 @@ router.post("*", function(req, res, next) {
     next();
 })
 
-router.post("/waiting", protected, function(req, res) {
+router.post("/waiting", guard, function(req, res) {
     hero.onWaiting(req.body.info);
     res.send({ success: true });
 });
 
-router.post("/starting", protected, function(req, res) {
+router.post("/starting", guard, function(req, res) {
     hero.onGameStarting(req.body.info);
     res.send({ success: true });
 });
 
-router.post("/round", protected, function(req, res) {
+router.post("/round", guard, function(req, res) {
     hero.onNewRound(req.body.info);
     res.send({ success: true });
 });
 
-router.post("/analysis", protected, function(req, res) {
+router.post("/analysis", guard, function(req, res) {
     hero.onAnalysis(req.body.info);
     res.send({ success: true });
 });
 
-router.post("/prediction", protected, function(req, res) {
+router.post("/prediction", guard, function(req, res) {
     hero.onPrediction(req.body.info);
     res.send({ success: true });
 });
 
-router.post("/answers", protected, function(req, res) {
+router.post("/answers", guard, function(req, res) {
     hero.onRoundOver(req.body.info);
     res.send({ success: true });
 });
 
-router.post("/ended", protected, function(req, res) {
+router.post("/ended", guard, function(req, res) {
     hero.onGameFinished(req.body.info);
     res.send({ success: true });
 });
