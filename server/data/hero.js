a0a7b2b584201050bbfe8f847cba453f9602fd20
Oliver Bell <freshollie@gmail.com>
2018-06-28 13:42:16 +0200

    Fix to socket server on non-chrome firefox

--- server/data/hero.js
+++ server/data/hero.js
@@ -37,26 +37,7 @@ class Hero {
     initialiseSocket(server) {
         this._socketServer = new WebSocket.Server({
             path:"/socket/hero",
-            server: server,
-            perMessageDeflate: {
-                zlibDeflateOptions: { // See zlib defaults.
-                    chunkSize: 1024,
-                    memLevel: 7,
-                    level: 3,
-                },
-                zlibInflateOptions: {
-                    chunkSize: 10 * 1024
-                },
-                // Other options settable:
-                clientNoContextTakeover: true, // Defaults to negotiated value.
-                serverNoContextTakeover: true, // Defaults to negotiated value.
-                clientMaxWindowBits: 10,       // Defaults to negotiated value.
-                serverMaxWindowBits: 10,       // Defaults to negotiated value.
-                // Below options specified as default values.
-                concurrencyLimit: 10,          // Limits zlib concurrency for perf.
-                threshold: 1024,               // Size (in bytes) below which messages
-                                                // should not be compressed.
-            }
+            server: server
         });
 
         this._socketServer.on('connection', (client) => {
