30513242ec728fb3a2a72022ac0d0e9000edf733
Oliver Bell <freshollie@gmail.com>
2018-06-30 19:50:22 +0200

    Made guard more efficient

--- server/authentication/guard.js
+++ server/authentication/guard.js
@@ -1,18 +1,24 @@
 
 const dns = require("dns");
-
-const PROXY_DNS_NAME = "proxy";
+const allowedIps = new Set();
 
 module.exports = function(req, res, next) {
     /**
-     * Only allow ips through which are not either
-     * local, or not from the proxies.
+     * Only allow ips which resolve to a domain name.
      * 
-     * Kinda a hack.
+     * As this service is behind a docker ingress network and proxy,
+     * the ips seen from the server will never be local if the ip 
+     * is remote.
+     * 
+     * This is a kinda hack. Future will move this service inwards
      */
     const remote = req.ip.split(":").pop();
 
     if (remote == "1" || remote == "127.0.0.1") {
+        return next();
+    }
+
+    if (allowedIps.has(remote)) {
         return next();
     }
 
@@ -24,6 +30,7 @@ module.exports = function(req, res, next) {
     dns.reverse(remote, function(err, domains) {
         if (!err && domains.length == 1) {
             // Is inside our docker network, as it has an address
+            allowedIps.add(remote);
             return next();
         }
         console.log(`${remote} blocked from setting data. Not a local connection`);
