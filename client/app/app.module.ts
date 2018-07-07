52cb668d8f1496b2c44cf536c35a0ca267825292
Oliver Bell <freshollie@gmail.com>
2018-07-07 03:53:47 +0200

    More fixs

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -3,9 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeroComponent } from './hero/hero.component';
-import { MatCardModule } from '@angular/material/card';
 import { ParticlesModule } from 'angular-particle';
-import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
@@ -17,9 +15,7 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
-    MatCardModule,
     ParticlesModule,
-    HttpClientModule,
     BrowserAnimationsModule,
     ServiceWorkerModule.register('./ngsw-worker.js', { enabled: environment.production })
   ],
