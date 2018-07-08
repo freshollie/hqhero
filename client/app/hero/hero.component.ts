cdaab53cb965bad7f2fc257ed79984d1c184101a
Oliver Bell <freshollie@gmail.com>
2018-07-08 16:57:30 +0200

    Refactor

--- client/app/hero/hero.component.ts
+++ client/app/hero/hero.component.ts
@@ -1,6 +1,107 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ApplicationRef } from '@angular/core';
 import { trigger, style, transition, animate, keyframes, query } from '@angular/animations';
 import * as moment from 'moment';
+
+/**
+ * The connection to the hero
+ */
+class HeroSocket {
+  private static path = "socket/hero";
+
+  /**
+   * Get the full socket path using the
+   * current host and the protocol
+   */
+  private static getSocketURI(): string {
+    let loc = window.location, uri;
+    
+    // When testing we, the socket is reconfigured based
+    // on our protocol
+    if (loc.protocol === "https:") {
+        uri = "wss://";
+    } else {
+      uri = "ws://";
+    }
+    uri += loc.host + window.location.pathname + HeroSocket.path;
+    return uri;
+  }
+
+  // Represents if a connection is currently present
+  private connection = false;
+  private reconnectAttempts = 0;
+
+  private onStateCallback;
+  private onConnectionLostCallback;
+  private onConnectedCallback;
+
+  constructor(
+      onStateCallback: Function, 
+      connectedCallback: Function, 
+      connectionLostCallback: Function
+    ) {
+    this.onStateCallback = onStateCallback;
+    this.onConnectedCallback = connectedCallback;
+    this.onConnectionLostCallback = connectionLostCallback;
+  }
+
+  /**
+   * Initialise the connection, reconnecting
+   * after connection failures
+   */
+  public initialise(): void {
+    this.connect();
+  }
+
+  /**
+   * Connect to the socket, reconnecting
+   * on failure
+   */
+  private connect() {
+    const socket = new WebSocket(HeroSocket.getSocketURI());
+
+    socket.onopen = () => {
+      this.onOpen();
+    }
+
+    socket.onmessage = (message) => {
+      this.onMessage(message);
+    };
+
+    socket.onclose = () => {
+      this.onClose();
+    }
+  }
+
+  private onOpen() {
+    this.reconnectAttempts = 0;
+    this.connection = true;
+    this.onConnectedCallback();
+  }
+
+  private onMessage(message: Object) {
+    if (message["data"]) {
+      this.onStateCallback(JSON.parse(message["data"]));
+    }
+  }
+
+  /**
+   * When the connection closes, try
+   * to re-initialise 5 times, before
+   * emmiting a connection lost event
+   */
+  private onClose() {
+    if (!this.connection) {
+      this.reconnectAttempts += 1;
+      if (this.reconnectAttempts > 5) {
+        this.onConnectionLostCallback();
+      }
+      setTimeout(() => {this.connect()}, 1000);
+    } else {
+      this.connection = false;
+      this.initialise();
+    }
+  }
+}
 
 @Component({
   selector: 'app-hero',
@@ -18,72 +119,46 @@ import * as moment from 'moment';
     ]),
   ]
 })
-
 export class HeroComponent implements OnInit {
 
-  public status = "";
+  public STATE_NONE = "";
+  public STATE_INITIALISING = "initialising";
+  public STATE_WAITING = "waiting";
+  public STATE_STARTING = "starting";
+  public STATE_THONKING = "thonking";
+  public STATE_ANSWERED = "answered";
+
+  public state = this.STATE_NONE;
+  public gameActive = false;
+
+  // How long (In english) until the next game
   public wait = "";
+
+  // The prize for the next game (Comes with the currency)
   public prize = "0";
+
+  // The time of the next game
   public nextGameTime = null;
   public question = "";
-  public questionNum = "-";
+
+  // The question number for this game
+  public questionNum = null;
+
   public choices: Object[] = [];
   public analysis: Object = {};
+
   public blinkState = "inactive";
 
-  public objectKeys = Object.keys;
-  
+  private heroSocket = new HeroSocket(
+    (state: Object) => {this.onHeroData(state)}, 
+    () => {this.onConnected()},
+    () => {this.onConnectionLost()}
+  );
+
   // Represents the connection status to the front-end
   public connected = false;
 
-  // Represents if a connection is currently present
-  private connection = false;
-  private reconnectAttempts = 0;
-
-  private socket: WebSocket;
-
-  constructor() {}
-  
-  private getWebsocketUri(): string {
-    let loc = window.location, uri;
-    if (loc.protocol === "https:") {
-        uri = "wss://";
-    } else {
-      uri = "ws://";
-    }
-    uri += loc.host + window.location.pathname + "socket/hero";
-    return uri;
-  }
-
-  private connect() {
-    this.socket = new WebSocket(this.getWebsocketUri());
-
-    this.socket.onopen = () => {
-      this.reconnectAttempts = 0;
-      this.connected = true;
-      this.connection = true;
-    }
-
-    this.socket.onmessage = (message) => {
-      if (message["data"]) {
-        this.onData(JSON.parse(message["data"]));
-      }
-    };
-
-    this.socket.onclose = () => {
-      if (!this.connection) {
-        this.reconnectAttempts += 1;
-        if (this.reconnectAttempts > 5) {
-          this.connected = false;
-          this.status = "";
-        }
-        setTimeout(() => {this.connect()}, 1000);
-      } else {
-        this.connection = false;
-        this.connect();
-      }
-    }
-  }
+  constructor(private applicationRef: ApplicationRef) {}
 
   ngOnInit(): void {
     // Testing waiting status
@@ -93,55 +168,92 @@ export class HeroComponent implements OnInit {
     this.wait = 'in 5 hours';
     this.prize = '$400,000';
     */
-
-    this.connect();
+    this.heroSocket.initialise();
     this.blinkLoop(); 
+
+    /* this.applicationRef.isStable.subscribe((stable) => {
+      if (stable && !initialised) {
+        initialised = true;
+        this.heroSocket.connect();
+        this.blinkLoop(); 
+      }
+    }); */
   }
 
-  onData(data): void {
-    this.status = data["status"];
-    if (this.status == "connecting") {
-      return;
-    }
+  private resetGame(): void {
+    this.questionNum = null;
+    this.question = "";
+    this.choices = null;
+  }
 
-    if (this.status == "waiting") {
-      if (data["info"]["nextGame"]) {
-        this.wait = moment(data["info"]["nextGame"]).fromNow();
-        this.nextGameTime = data["info"]["nextGame"];
-        this.prize = data["info"]["prize"];
-      } else {
-        this.wait = null;
-        this.nextGameTime = null;
-        this.prize = null;
-      }
-    }
+  private onConnected(): void {
+    this.connected = true;
+  }
+
+  private onConnectionLost(): void {
+    this.state = this.STATE_NONE;
     
-    if (data["game"] && data["game"]["round"]) {
-      // this.analysis = {}
-      if (data["game"]["round"]["question"]) {
-        // If question is new, re-assign all. Otherwise, update choices.
-        if (data["game"]["round"]["question"] != this.question) {
+    this.connected = false;
+  }
 
-          this.questionNum = data["game"]["round"]["num"];
-          this.question = data["game"]["round"]["question"];
-          this.choices = data["game"]["round"]["choices"];
-        } else {
-          
-          for (let choiceNum in this.choices) {
-            for (let property in data["game"]["round"]["choices"][choiceNum]) {
-              this.choices[choiceNum][property] =  data["game"]["round"]["choices"][choiceNum][property];
-            }
+  private onHeroData(data: object): void {
+    this.state = data["status"];
+
+    if (this.state == this.STATE_INITIALISING) {
+      // Waiting for the backend to connect to HQ
+      this.resetGame();
+      this.gameActive = false;
+    } else if (this.state == this.STATE_WAITING) {
+      // Waiting for the next game
+      this.resetGame();
+      this.onWaiting(data);
+    } else if (data["game"] && data["game"]["round"]) {
+      // Round information has game through
+      this.onRoundData(data["game"]["round"]);
+    }
+  }
+
+  private onWaiting(data: object): void {
+    this.gameActive = false;
+
+    if (data["info"]["nextGame"]) {
+      this.wait = moment(data["info"]["nextGame"]).fromNow();
+      this.nextGameTime = data["info"]["nextGame"];
+      this.prize = data["info"]["prize"];
+    } else {
+      this.wait = null;
+      this.nextGameTime = null;
+      this.prize = null;
+    }
+  }
+
+  private onRoundData(round: object): void {
+    this.gameActive = true;
+      // this.analysis = {}
+    if (round["question"]) {
+      // If question is new, re-assign all. Otherwise, update choices.
+      // The choices include the predictions for each choice
+      // When the prediction comes through
+      if (round["question"] != this.question) {
+        this.questionNum = round["num"];
+        this.question = round["question"];
+        this.choices = round["choices"];
+      } else {
+        for (let choiceNum in this.choices) {
+          for (let property in round["choices"][choiceNum]) {
+            this.choices[choiceNum][property] =  round["choices"][choiceNum][property];
           }
         }
       }
+    }
 
-      if (data["game"]["round"]["analysis"]) {
-        this.analysis = data["game"]["round"]["analysis"];
-      }
+    // Analysis of the current question
+    if (round["analysis"]) {
+      this.analysis = round["analysis"];
     }
   }
 
-  public getBackgroundPosition(choice) {
+  public calculateChoiceBackgroundPosition(choice) {
     if (choice.prediction == undefined) {
       return '100% 100%';
     }
@@ -149,7 +261,7 @@ export class HeroComponent implements OnInit {
     return value + '% 100%';
   }
 
-  public isCorrect() {
+  public wasCorrect() {
     for (let choice of this.choices) {
       if (choice['best'] && choice['correct']) {
         return true;
@@ -158,7 +270,7 @@ export class HeroComponent implements OnInit {
     return false;
   }
 
-  public isIncorrect() {
+  public wasIncorrect() {
     for (let choice of this.choices) {
       if (choice['correct']) {
         if (!choice['best']) {
@@ -173,14 +285,14 @@ export class HeroComponent implements OnInit {
 
   private blinkLoop() {
     setTimeout(() => {
-      if (this.status != 'waiting') {
+      if (this.state != 'waiting') {
         this.blink();
       }
       this.blinkLoop();
     }, 2000 + Math.random() * 8000);
   }
 
-  public blink() {
+  private blink() {
     this.blinkState = 'active';
   }
 
