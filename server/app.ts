30dcdecdf88dae8719fb221f70b5a46543b77f70
Oliver Bell <freshollie@gmail.com>
2018-07-25 20:42:59 +0200

    Formatting

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,11 @@
 /**
  * MIT License
  *
- * Copyright (c) Oliver Bell <freshollie@gmail.com> 
+ * Copyright (c) Oliver Bell  <freshollie@gmail.com> 
  *             & Eddie Reeder <edlilkid@hotmail.co.uk>
  *
  */
+
 import express from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
